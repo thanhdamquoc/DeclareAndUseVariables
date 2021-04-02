ee1b49ec9bb445bf39b80ce4030e576d50a07cc1
thanhdamquoc <79950542+thanhdamquoc@users.noreply.github.com>
2021-04-02 11:48:50 +0700

    add files

---
+++ src/DeclareAndUseVariables.java
@@ -0,0 +1,23 @@
+public class DeclareAndUseVariables {
+    public static void main(String[] args) {
+//        Biến i kiểu số nguyên, có giá trị là 10
+//        Biến f kiểu số thực, có giá trị là 20.5
+//        Biến d kiểu số thực lớn, có giá trị là 20.5
+//        Biến b kiểu logic, có giá trị là true
+//        Biến c kiểu ký tự, có giá trị là 'a'
+//        Biến s kiểu chuỗi, có giá trị là "Hà Nội".
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5d;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
